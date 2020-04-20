111ac34a01b29441d78eb8209a17ee510e5b32e8
lauraBrasileiro <laurabrasileiro.c@gmail.com>
2020-04-20 11:58:25 -0300

    aula2

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FeedComponent } from './feed/feed.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FeedComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
