b78040df714173756122615eb462504904a7c703
lauraBrasileiro <laurabrasileiro.c@gmail.com>
2020-04-21 13:03:31 -0300

    Criação do EndPoint getAllPostagens

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { threadId } from 'worker_threads';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllPostagens(){
+    return this.http.get('http://31.220.57.14:8080/postagens')
+  }
+
+
+
 }
